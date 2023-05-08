0d8c188fcd9fc5feff07fba51a0a33654e91b8cb
deivid <101598816+deividbautista@users.noreply.github.com>
2023-05-08 16:49:49 -0500

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -1,4 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+template_dir = os.path.join(template_dir, 'src', 'templates')
 
+app = Flask(__name__,template_folder = template_dir)
+
+#Rutas
+@app.route('/')
+def home():
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
