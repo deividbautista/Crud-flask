3059aa7c52fdbc21af38c5425630ffb48c226db7
deivid <101598816+deividbautista@users.noreply.github.com>
2023-05-08 17:39:00 -0500

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 import os
-
+import database as db
 template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 template_dir = os.path.join(template_dir, 'src', 'templates')
 
@@ -9,8 +9,16 @@ app = Flask(__name__,template_folder = template_dir)
 #Rutas
 @app.route('/')
 def home():
-
-    return render_template('index.html')
+    Cursor = db.database.cursor()
+    Cursor.execute("SELECT * FROM users")
+    myresult = Cursor.fetchall()
+    #
+    InsertObject = []
+    columnNames = [column[0] for column in Cursor.description]
+    for record in myresult:
+        InsertObject.append(dict(zip(columnNames, record)))
+    Cursor.close()
+    return render_template('index.html', data=InsertObject)
 
 if __name__ == '__main__':
     app.run(debug=True)
