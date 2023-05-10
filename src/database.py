3cec010348fa8c4763039312da6caf3b9b604be6
deivid <101598816+deividbautista@users.noreply.github.com>
2023-05-10 17:34:09 -0500

    documentation

--- src/database.py
+++ src/database.py
@@ -1,5 +1,9 @@
+#Importamos la libreria de mysql, que utilizaremos para poder
+#realizar consultas SQL
 import mysql.connector
 
+#Tenemos la configuración sobre la base de datos para poder
+#realizar la conección.
 database = mysql.connector.connect(
     host='localhost',
     user='root',
